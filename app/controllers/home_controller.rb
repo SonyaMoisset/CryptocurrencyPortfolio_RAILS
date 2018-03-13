f7b3023df350c5711327a939ee92a3aab5dc6d5b
smoisset <smoisset@worldremit.com>
2018-03-13 22:08:39 +0000

    Add CoinMarketCap API call

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   def index
+    require 'net/http'
+    require 'json'
+
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @coins = JSON.parse(@response)
   end
 
   def about
